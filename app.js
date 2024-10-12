79d522c4772a56cc2c33e5699d6c0fd7acc73f1c
yael <howitzer117@outlook.com>
2024-10-12 15:49:25 -0600

    pruebas desde git page

--- app.js
+++ app.js
@@ -12,18 +12,16 @@ const app = express();
 const port = 3000;
 
 // habilita cors
-
+const allowedOrigins = [
+  'http://127.0.0.1:5500', // Permitir solicitudes desde tu frontend local
+  'https://elpuzzupuzu.github.io/frontbikelike/' // Permitir solicitudes desde tu dominio GitHub Pages
+];
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500', // Permitir solicitudes desde tu frontend local
-  methods: ['GET', 'POST'], // Métodos que deseas permitir
+  origin: allowedOrigins, // Permitir solicitudes desde los orígenes especificados
+  methods: ['GET', 'POST', 'PUT'], // Métodos que deseas permitir (agregué PUT aquí ya que lo usas)
   credentials: true // Permitir cookies (si es necesario)
 }));
-
-
-
-
-
 
 // Middleware para Morgan: registro de solicitudes HTTP
 app.use(morgan('dev')); // Modo 'dev' muestra logs concisos de las solicitudes
@@ -32,12 +30,9 @@ app.use(morgan('dev')); // Modo 'dev' muestra logs concisos de las solicitudes
 app.use(express.json());
 
 // Rutas de bicicletas
-
-
 app.get('/bicicletas', authMiddleware, (req, res) => bicicletaController.obtenerBicicletas(req, res));
 app.get('/bicicletas/:modelo', authMiddleware, (req, res) => bicicletaController.obtenerBicicletaPorModelo(req, res));
 app.get('/bicicletas/id/:id', authMiddleware, (req, res) => bicicletaController.obtenerBicicletaPorId(req, res));
-
 
 // Aplica el middleware de validación aquí
 app.post('/bicicletas', validarBicicleta, (req, res) => bicicletaController.crearBicicleta(req, res));
